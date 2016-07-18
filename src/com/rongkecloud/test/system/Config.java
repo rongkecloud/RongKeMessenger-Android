5a865c78127d5280e0a447c5ecfb7fc3ed96056f
许晓萌 <xuxiaomeng@rongkecloud.com>
2016-07-18 18:10:31 +0800

    +

--- src/com/rongkecloud/test/system/Config.java
+++ src/com/rongkecloud/test/system/Config.java
@@ -76,18 +76,7 @@ public class Config {
 	public String getString(String key, String defaultValue) {
 		return mSp.getString(key, defaultValue);
 	}
-	public Set<String> getStringSet(String key)
-	{
-		return mSp.getStringSet(key,null);
-	}
 
-	public void setStringSet(String key,Set<String> results)
-	{
-		mSp.edit().remove(key).commit();
-		mSp.edit().putStringSet(key,results).commit();
-	}
-
-	
 	public boolean upgradeSP() {
 		if (getBoolean(ConfigKey.UPGRADE_AP, false))
 			return false;
